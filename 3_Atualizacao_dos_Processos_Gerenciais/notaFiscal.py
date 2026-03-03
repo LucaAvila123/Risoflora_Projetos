af03de1546e494a4155bba4f0290ed9c24020b42
LucaAvila123 <luca.avila@edu.ufes.br>
2026-03-02 23:56:14 -0300

    alguma coisa está caminhando para checagem de notas fiscais eletrônicas

--- 3_Atualizacao_dos_Processos_Gerenciais/notaFiscal.py
+++ 3_Atualizacao_dos_Processos_Gerenciais/notaFiscal.py
@@ -2,6 +2,17 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException, TimeoutException
+import re
+import undetected_chromedriver as uc
+
+def initialize_session():
+        try: 
+            sess = uc.Chrome()
+        except Exception as e: 
+            main_version_string = re.search(r"Current browser version is (\d+\.\d+\.\d+)", str(e)).group(1)
+            main_version = int(main_version_string.split(".")[0])
+            sess = uc.Chrome(version_main=main_version)
+        return sess
 
 def elemento_existe(driver, by, seletor):
     """Verifica se um elemento existe sem lançar exceção"""
@@ -14,7 +25,8 @@ def elemento_existe(driver, by, seletor):
 def first_test_script(numero_nota_fiscal):
     # Create an instance of the Chrome WebDriver
     # you can use other browsers too
-    driver = webdriver.Chrome()
+    
+    driver = initialize_session()
     
     # navigate to the website
     driver.get("https://app.sefaz.es.gov.br/ConsultaNFCe/")
@@ -25,17 +37,17 @@ def first_test_script(numero_nota_fiscal):
     # Print the title of the website
     print("Title: " + title)
     campo_input = driver.find_element(By.NAME, "ctl00$body$txtConsulta")
-    print("Campo input: ", campo_input)
-
+    print("Campo input: ", campo_input)    
     campo_input.send_keys(numero_nota_fiscal)
     
     time.sleep(3)
-    # Close the browser window
+
+    botao_input = driver.find_element(By.NAME, "ctl00$body$Button1")
+    botao_input.click()
+    time.sleep(3)
+
     driver.quit()
-    
-
-
 
 if     __name__ == "__main__":
-    nota_fiscal = input("Insira numero da nota Fiscal no formato apropriado: ")
+    # nota_fiscal = input("Insira numero da nota Fiscal no formato apropriado: ")
     first_test_script(nota_fiscal)
